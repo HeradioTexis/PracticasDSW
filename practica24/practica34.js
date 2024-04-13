a25ce372a790f39ec043133008a73422dace4740
HeradioTexis <159208364+HeradioTexis@users.noreply.github.com>
2024-04-12 19:38:27 -0700

    practica 24

---
+++ practica24/practica34.js
@@ -0,0 +1,14 @@
+
+
+var hayUnHuracan = new Boolean(false);
+
+ huracan(hayUnHuracan);
+
+function huracan (hayUnHuracan){
+
+    if(hayUnHuracan == true){
+        return alert("Hay un huracan.");
+    }else{
+        return alert("Esta Soleado");
+    }
+}
